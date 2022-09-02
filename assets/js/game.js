4ba09939be111f551484e5b890956688a7e451d3
Mpergolatti <Mpergolatti@gmail.com>
2022-09-02 13:19:05 -0400

    added first variable and function

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,10 @@
-window.alert("this is an alert! Javascript is running!");
+// will prompt user to enter name and alert the name.
+var playerName = window.prompt("what is your robots name");
+    window.alert(playerName);
+
+// creates a window alert saying the fight has begun
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+// fight();
